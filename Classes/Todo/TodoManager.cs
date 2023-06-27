fe312677e6d304fc9fcdaddfd82d2ae993ab0f75
Galaxy092 <096polideth@gmail.com>
2023-06-27 08:52:52 +0700

    update method name

--- Classes/Todo/TodoManager.cs
+++ Classes/Todo/TodoManager.cs
@@ -8,24 +8,24 @@ namespace C__Todo_App_with_SQL_Server.Classes
     public class TodoManager
     {
         // Update Todo
-        public void vUpdateData(string _id)
+        public void updateTodo(string _id)
         {
             string sql = " usp_TodoList '" + _id + "', '', '', '', '', 'UPDATE'";
-            DataSet ds = DBConnection.vFillDataSet(sql);
+            DataSet ds = DBConnection.GetConnection(sql);
         }
 
         // Delete Todo
-        public void vDeleteData(string _id)
+        public void deleteTodo(string _id)
         {
             string sql = " usp_TodoList '" + _id + "', '', '', '', '', 'DELETE'";
-            DataSet ds = DBConnection.vFillDataSet(sql);
+            DataSet ds = DBConnection.GetConnection(sql);
         }
 
         // Add Todo
-        public void vAddData(TextBox txtTitle, DateTimePicker dtpDate, TextBox txtDescription)
+        public void addTodo(TextBox txtTitle, DateTimePicker dtpDate, TextBox txtDescription)
         {
             string sql = " usp_TodoList '', '" + txtTitle.Text + "', '" + DateTime.Parse(dtpDate.Value.ToString()) + "', '', '" + txtDescription.Text + "', 'ADD'";
-            DataSet ds = DBConnection.vFillDataSet(sql);
+            DataSet ds = DBConnection.GetConnection(sql);
         }
     }
 }
