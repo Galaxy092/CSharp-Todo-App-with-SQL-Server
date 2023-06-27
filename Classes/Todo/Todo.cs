b36d1727e723fa35e42c5400fffbad8988cae053
Galaxy092 <096polideth@gmail.com>
2023-06-27 08:53:05 +0700

    update class

--- Classes/Todo/Todo.cs
+++ Classes/Todo/Todo.cs
@@ -1,23 +1,28 @@
-﻿namespace C__Todo_App_with_SQL_Server.Classes
+﻿using System;
+
+namespace C__Todo_App_with_SQL_Server.Classes
 {
     public class Todo
     {
-        // Field
-        private string title;
-        private string description;
+        public int TodoID { get; set; }
 
-        // Default Constructor
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string completed { get; set; }
+
+        public string Description { get; set; }
+
         public Todo() { }
 
-        // COnstrutor with parameter
-        public Todo(string title, string description)
+        public Todo(int todoID, string title, DateTime date, string completed, string description)
         {
+            TodoID = todoID;
             Title = title;
+            Date = date;
+            this.completed = completed;
             Description = description;
         }
-
-        // Property
-        public string Title { get => title; set => title = value; }
-        public string Description { get => description; set => description = value; }
     }
 }
